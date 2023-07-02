c78cc86a29dc2b01c43d81b27928e6aa8207af5b
JacquesIversen <jackiracki1@gmail.com>
2023-07-02 20:42:37 +0000

    changed localhost

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django2.herokuapp.com", "localhost", '8000-jacquesiver-djangoblog2-u1kkjuncqzl.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ["django2.herokuapp.com", "localhost",
+"django2-f8b3c874795f.herokuapp.com", "8000-jacquesiver-djangoblog2-u1kkjuncqzl.ws-eu101.gitpod.io"]
 
 
 # Application definition
